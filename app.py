5d14018b77e18bf37eb4124072b8149397217e89
srchby <raidentsuji@gmail.com>
2025-06-11 18:25:18 -0300

    flask login

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
-from flask import Flask, render_template, request, redirect
-from models import db, Client, Order
+from flask import Flask, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from models import db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///<nome>.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///redaplay.db'
 db.init_app(app)
 
 with app.app_context():
     db.create_all()
     db.session.commit()
     
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    error = None
+    if request.method == 'POST':
+        action = request.form.get('action')
+        username = request.form['username']
+        password = request.form['password']
+        if action == 'login':
+            user = User.query.filter_by(username=username).first()
+            if user and check_password_hash(user.password, password):
+                session['user_id'] = user.id
+                return redirect('/inicio')
+            else:
+                error = 'Credenciais inválidas.'
+        elif action == 'register':
+            if User.query.filter_by(username=username).first():
+                error = 'Nome de usuário já existente.'
+            else:
+                hashed_pw = generate_password_hash(password)
+                new_user = User(username=username, password=hashed_pw)
+                db.session.add(new_user)
+                db.session.commit()
+                session['user_id'] = new_user.id
+                return redirect('/inicio')
+    return render_template('index.html', error=error)
+
+@app.route('/inicio')
+def main():
+    if 'user_id' in session:
+        return render_template('index.html')
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect('/')
+
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -2,15 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Teacher(db.Model):
-    
-
-class Student(db.Model):
-    
-    
-class Lesson(db.Model):
-    
-    
-class Subject(db.Model):
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
     
     
